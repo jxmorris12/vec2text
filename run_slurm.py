cb1330ec00dd9adfc5a787c6e92fb666f27b3654
Jack Morris <jxmorris12@gmail.com>
2023-04-28 14:19:00 -0400

    restore slurm script for a 2-week job

--- run_slurm.py
+++ run_slurm.py
@@ -4,7 +4,9 @@ from datetime import datetime
 from slurmpy import Slurm
 
 BASE_PYTHON_CMD = """
-python run.py --per_device_train_batch_size {batch_size} \
+python run.py \
+--experiment inversion \
+--per_device_train_batch_size {batch_size} \
 --per_device_eval_batch_size {batch_size} \
 --max_seq_length {max_seq_length} \
 --model_name_or_path {model_name} \
@@ -15,26 +17,26 @@ python run.py --per_device_train_batch_size {batch_size} \
 --learning_rate {learning_rate} \
 --freeze_strategy {freeze_strategy} \
 --embedder_fake_with_zeros {embedder_fake_with_zeros} \
+--randomly_truncate_train_inputs {truncate} \
 --use_frozen_embeddings_as_input False \
 --encoder_dropout_disabled False \
 --decoder_dropout_disabled False \
 --use_less_data 0 \
---num_train_epochs 24 \
+--num_train_epochs 100 \
 --max_eval_samples 500 \
---eval_steps 2500000000000 \
+--eval_steps 25000 \
 --warmup_steps 100000 \
 --bf16=1 \
 --use_lora=0 \
 --use_wandb=1
 """
 
-
 models = [
     # 't5-small',
-    # 't5-base',
+    't5-base',
     # 't5-large',
     # 't5-3b',
-    "t5-11b",
+    # "t5-11b",
 ]
 
 # emb_models = ['dpr', 'ance_tele']
@@ -56,30 +58,30 @@ emb_models = ["gtr_base"]
 # exp_group_name = 'mar17-baselines'
 # exp_group_name = 'mar19-random'
 # exp_group_name = 'mar21-bn-drop'
-exp_group_name = "apr16-huge"
+# exp_group_name = "apr16-huge"
+exp_group_name = "apr27-baselines"
 ##########################################
 
-# batch_size = 128
-batch_size = 32
+batch_size = 128
+# batch_size = 32
 
 # max_seq_length = [1+1, 4+1, 8+1, 64+1]
 # max_seq_length = [8, 32, 128]
-max_seq_length = [8, 32, 128]
+max_seq_length = [64]
 
 embedder_no_grad = [True]
 # embedder_no_grad = [True, False]
 
-# learning_rates = [1e-4, 5e-4, 1e-3, 5e-3]
-# learning_rates = [5e-4]
-learning_rates = [2e-4]
+learning_rates = [1e-3]
 
 num_repeat_tokens = [16]
-# num_repeat_tokens = [1, 2, 4, 8, 16, 32, 64, 128]
 
 freeze_strategies = ["none"]
 # freeze_strategies = ["decoder", "encoder_and_decoder", "encoder", "none"]
 
 fake_embedding_with_zeros = [False]  # embedder_fake_with_zeros
+
+do_truncation = [True, False]
 
 ACTUALLY_RUN_COMMAND = True
 
@@ -130,8 +132,9 @@ for args in itertools.product(
     embedder_no_grad,
     freeze_strategies,
     fake_embedding_with_zeros,
+    do_truncation,
 ):
-    m, e, lr, n, msl, eng, frs, emb_fake = args
+    m, e, lr, n, msl, eng, frs, emb_fake, truncate = args
     total += 1
     cmd = BASE_PYTHON_CMD.format(
         batch_size=batch_size,
@@ -147,7 +150,9 @@ for args in itertools.product(
         #
         exp_group_name=exp_group_name,
         freeze_strategy=frs,
+        truncate=truncate,
     )
+    cmd = cmd.replace("\n", " ")
     job_desc = ".".join(map(str, args))
     run_cmd(cmd, job_desc=job_desc)
 
