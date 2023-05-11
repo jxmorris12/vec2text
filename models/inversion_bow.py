fix off-by-one in set-of-words model

models/inversion_bow.py
@@ -55,7 +55,7 @@ class InversionModelBagOfWords(nn.Module):
         # TODO implement different generation strategies
 
         max_length = inputs.get("input_ids", inputs["embedder_input_ids"]).shape[1]
-
+        # Take top 32
         return logits.topk(max_length, dim=1).indices
 
     def call_embedding_model(
@@ -129,7 +129,7 @@ class InversionModelBagOfWords(nn.Module):
             inputs_embeds=inputs_embeds,
             attention_mask=attention_mask,
         )
-        outputs = {"logits": logits[:, 1:]}  # hf trainer output format
+        outputs = {"logits": logits}  # hf trainer output format
         if labels is not None:
             labels = torch.cat(
                 (

trainers/base.py
@@ -251,7 +251,7 @@ class BaseTrainer(transformers.Trainer):
             )
             labels_emb = self.call_embedding_model(
                 input_ids=preds_sample_labels,
-                attention_mask=(preds_sample_labels != pad_token_id),
+                attention_mask=torch.ones_like(preds_sample_labels),
             )
             emb_cos_sim = (
                 torch.nn.CosineSimilarity(dim=1)(preds_emb, labels_emb).mean().item()