05789ba9a199cad1bb1c5389d445882b32dd138f
Jack Morris <jxmorris12@gmail.com>
2023-06-20 11:28:00 -0400

    update slurm params for precompute

--- experiment_scripts/run_slurm_precompute_train_hypotheses.py
+++ experiment_scripts/run_slurm_precompute_train_hypotheses.py
@@ -26,14 +26,14 @@ def run_cmd(cmd: str, job_desc: str):
         slurm = Slurm(
             job_name,
             slurm_kwargs={
-                "partition": "rush",
-                "gres": "gpu:a6000:4",
+                "partition": "gpu,rush",
+                "gres": "gpu:a6000:1",
                 # "gres": "gpu:1",
                 # "constraint": "a40|3090|a6000|a5000|a100-40",
                 "ntasks": 1,
-                "cpus-per-task": 16,
-                "mem": "100G",
-                "nodelist": "rush-compute-02",
+                "cpus-per-task": 4,
+                "mem": "48G",
+                # "nodelist": "rush-compute-02",
                 # "time": "24:00:00",
                 # "time": "72:00:00",
                 "time": "168:00:00",  # 168 hours --> 1 week
