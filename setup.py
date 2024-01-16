6a7efbdd2d823e564c3e3e02f54af1df1cd3dd7d
Jack Morris <jxmorris12@gmail.com>
2024-01-16 15:25:33 -0500

    update master

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="vec2text",
-    version="0.0.9",
+    version="0.0.10",
     description="convert embedding vectors back to text",
     author="Jack Morris",
     author_email="jxm3@cornell.edu",
